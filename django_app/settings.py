ae8c66b7241def6c199c668cb39fe8c257071308
Iacopo454 <iacopo.gentile@icloud.com>
2021-06-06 18:56:17 +0000

    changes made to setting.py

--- django_app/settings.py
+++ django_app/settings.py
@@ -36,9 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.auth',
-    'django.contrib.messages',
-    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 
 ]
 
